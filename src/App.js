7a996ed1e974ceb9293b40d126e4b51f090e9ea1
Alexis Dela Rosa <delarosauberstrike@gmail.com>
2024-11-19 20:29:04 +0800

    Added landing component transition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, {useEffect,useState} from 'react';
 import './App.css';
+import { CSSTransition } from 'react-transition-group';
 
 import Index from './components/Index.jsx';
 import Landing from './components/Landing.jsx';
@@ -11,14 +12,29 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowHomepage(true);
-    }, 7000); 
+    }, 5000); 
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className='App'>
-      {!showHomepage? <Landing/> : <Index bgcolor="black"/>}
+      <CSSTransition
+        in={!showHomepage}
+        timeout={500}
+        classNames="fade"
+        unmountOnExit
+      >
+        <Landing />
+      </CSSTransition>
+      <CSSTransition
+        in={showHomepage}
+        timeout={500}
+        classNames="fade"
+        unmountOnExit
+      >
+        <Index bgcolor="black" />
+      </CSSTransition>
     </div>
   );
 }
